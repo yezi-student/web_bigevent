6fff2840e5ddcdf6ca2997f48f3b2c7b78a87e18
叶子 <10150193+leaffffff@user.noreply.gitee.com>
2022-06-26 14:54:38 +0800

    login和reg的开发

---
+++ assets/js/login.js
@@ -0,0 +1,58 @@
+$(function(){
+    // 登录跳转注册逻辑
+    $("#login").on("click", function(){
+        $(".login-box").hide(),
+        $(".reg-box").show()
+    });
+     
+      // 注册跳转登录逻辑
+  $('#reg').on('click',function(){
+      $('.reg-box').hide(),
+      $(".login-box").show()
+  })
+//   表单验证规则
+// 从layui中获取form对象
+    let form =layui.form
+  form.verify({
+    username: function(value, item){ //value：表单的值、item：表单的DOM对象
+      if(!new RegExp("^[a-zA-Z0-9_\u4e00-\u9fa5\\s·]+$").test(value)){
+        return '用户名不能有特殊字符';
+      }
+      if(/(^\_)|(\__)|(\_+$)/.test(value)){
+        return '用户名首尾不能出现下划线\'_\'';
+      }
+      if(/^\d+\d+\d$/.test(value)){
+        return '用户名不能全为数字';
+      }
+      
+      //如果不想自动弹出默认提示框，可以直接返回 true，这时你可以通过其他任意方式提示（v2.5.7 新增）
+      if(value === 'xxx'){
+        alert('用户名不能为敏感词');
+        return true;
+      }
+    }
+    
+    //我们既支持上述函数式的方式，也支持下述数组的形式
+    //数组的两个值分别代表：[正则匹配、匹配不符时的提示文字]
+    ,pass: [
+      /^[\S]{6,12}$/
+      ,'密码必须6到12位，且不能出现空格'
+    ] 
+  }); 
+  // 监听注册表单的提交事件
+  $('#form_reg').on('submit',function(e){
+    // 阻止跳转
+    e.preventDefault();
+    console.log($('#form_reg[name=username]'));
+    $.post("http://127.0.0.1:3007/api/reguser", {username:'admin123',password:'admin123'},function(res){
+      console.log(res.message);
+     });
+  })
+  // 监听登录表单的提交事件
+  $('#form_login').on('submit',function(e){
+    e.preventDefault();
+    $.post("http://127.0.0.1:3007/api/login", {username:'admin123',password:'admin123'},function(res){
+      console.log(res.message);
+     });
+  })
+  });
